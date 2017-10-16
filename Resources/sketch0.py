e2a3f804133eaeaa75e9a059465fa71dd758cec5
AlbertoWilliams <32202484+AlbertoWilliams@users.noreply.github.com>
2017-10-16 08:43:08 -0400

    Create sketch0.py

---
+++ Resources/sketch0.py
@@ -0,0 +1,13 @@
+class neuron:
+  this.__init__(self, input0, weight0, input1, weight1):
+    self.input0 = input0
+    self.input1 = input1
+    self.weight0 = weight0
+    self.weight1 = weight1
+  def transferFunction():
+    #The transfer function definition
+  def activationFunction(transferFunctionValue):
+    if transferFunctionValue < umbral:
+      return 0
+    else:
+      return 1
